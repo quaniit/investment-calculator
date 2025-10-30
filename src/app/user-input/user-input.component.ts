0417f64e8eae68f3bb2415cf9d110271a7dad4c1
Jen Funck <Jennifer.Funck3581@gmail.com>
2025-10-30 00:11:13 -0400

    feat(user-input): form submission
    Added two-way binding

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  initialInvestment = '0';
+  annualInvestment = '0';
+  expectedReturn = '5';
+  duration = '10';
 
+  onSubmit() {}
 }
